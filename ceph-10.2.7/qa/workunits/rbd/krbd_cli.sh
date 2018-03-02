cffb0002dc973cdfaaf00d5a6a5ee2e448233bde
JiangYu <zglnsyyj@gmail.com>
2018-03-02 17:05:46 +0800

    update krbd_test

--- ceph-10.2.7/qa/workunits/rbd/krbd_cli.sh
+++ ceph-10.2.7/qa/workunits/rbd/krbd_cli.sh
@@ -2,13 +2,13 @@
 
 POOL_NAME="krbd-cli"
 IMAGE_NAME="image-cli"
+PG_NUM=128
 
 tear_down () {
   ceph osd pool delete $POOL_NAME $POOL_NAME --yes-i-really-really-mean-it || true
 }
 
 set_up () {
-  tear_down
   ceph osd pool create $POOL_NAME $PG_NUM
 }
 
@@ -16,25 +16,27 @@ trap tear_down EXIT HUP INT
 set_up
 
 # creating an image in a pool-managed snapshot pool should fail
-rbd create --pool $POOL_NAME --size 1 $IMAGE_NAME && exit 1 || true
+rbd create --pool $POOL_NAME --size 1 $IMAGE_NAME
 
 # list rbd 
 rbd list -p $POOL_NAME
 
 # rbd map
-rbd map $POOL_NAME/$IMAGE_NAME
+#rbd map $POOL_NAME/$IMAGE_NAME
 
 # rbd lock add
 rbd lock add $POOL_NAME/$IMAGE_NAME image-cli-lock
 
 # rbd lock list
-rbd lock ls -p $POOL_NAME
+rbd lock list $POOL_NAME/$IMAGE_NAME
+LOCK_CLIENT=`rbd lock list $POOL_NAME/$IMAGE_NAME | awk '{print $1}' | grep client`
 
 # rbd lock remove
-#rbd lock remove $POOL_NAME/$IMAGE_NAME image-cli-lock 
+rbd lock remove $POOL_NAME/$IMAGE_NAME image-cli-lock $LOCK_CLIENT 
+rbd lock list $POOL_NAME/$IMAGE_NAME
 
 # rbd unmap
-rbd unmap
+#rbd unmap
 
 
 echo OK
